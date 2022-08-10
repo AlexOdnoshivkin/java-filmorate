d23060045f99a9a8f79cb66e3bf7e86f92d7839d
Alex Odnoshivkin <alexander.odnoshivkin@yandex.ru>
2022-08-11 01:55:17 +0300

    feat: sprint 11 complete

--- src/main/java/ru/yandex/practicum/filmorate/storage/EntityStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EntityStorage.java
@@ -3,10 +3,10 @@ package ru.yandex.practicum.filmorate.storage;
 import java.util.List;
 
 // Базовый интерфейс для хранилищ. Создал, так как функции в них повторяются
-public interface BaseStorage<T> {
-    public void add(T t);
+public interface EntityStorage<T> {
+    public T add(T t);
 
-    public void update(T t);
+    public T update(T t);
 
     public void delete(T t);
 
