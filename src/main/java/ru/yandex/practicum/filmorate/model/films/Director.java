1f06f77e1d6158b125b2f87c1d69d13a3e91c63c
Alex Odnoshivkin <100222349+AlexOdnoshivkin@users.noreply.github.com>
2022-08-24 13:04:11 +0300

    fix: review 1

--- src/main/java/ru/yandex/practicum/filmorate/model/films/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/films/Director.java
@@ -3,12 +3,10 @@ package ru.yandex.practicum.filmorate.model.films;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Data
 public class Director {
     private long id;
-    @NotEmpty
     @NotBlank
     private String name;
 }
